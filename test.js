2f1f6032eae6d835afaf9b68352c62b42466b94b
Andreas Gillström <andreasgillstrom@gmail.com>
2015-11-04 19:53:35 +0100

    ES2015ify tests

--- test.js
+++ test.js
@@ -1,8 +1,7 @@
-'use strict';
-var test = require('ava');
-var arrayMin = require('./');
+import test from 'ava';
+import arrayMin from './';
 
-test(function (t) {
+test(t => {
 	t.is(arrayMin([5, 4, 3, 2, 1]), 1);
 	t.is(arrayMin([22, 44, 11, 66, 88]), 11);
 	t.is(arrayMin([1, 0.11, 2.25, 0.0051]), 0.0051);
